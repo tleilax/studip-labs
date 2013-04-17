84eafa00e51f0d0c6ee088befef0819087a07f2d
Jan-Hendrik Willms <tleilax+github@gmail.com>
2013-04-17 22:13:47 +0200

    change default status code

--- api/lib/api/RouterException.class.php
+++ api/lib/api/RouterException.class.php
@@ -11,7 +11,7 @@ use \Exception;
 
 class RouterException extends Exception
 {
-    public function __construct($message = '', $code = 400, $previous = null)
+    public function __construct($message = '', $code = 500, $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
